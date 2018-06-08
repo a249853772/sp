bca1cbfd02ca9fae11504d93f9940b0349ab0b7d
a249853772 <249853772@qq.com>
2018-06-08 14:21:13 +0800

    N

--- springboot/src/main/java/com/sys/main/controller/HelloController.java
+++ springboot/src/main/java/com/sys/main/controller/HelloController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,10 +17,18 @@ public class HelloController {
 		return "test";
 	}
 	
-	@RequestMapping("/index")
+	/*@RequestMapping("/index")
 	public String index(Model m){
 //		m.addAttribute("now", DateFormat.getInstance().format(new Date()));
 		return "index";
-	}
+	}*/
+	
+	@RequestMapping("/")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";  
+    }
 
 }
--- springboot/src/main/java/com/sys/main/exceptionHandler/GlobalExceptionHandler.java
+++ springboot/src/main/java/com/sys/main/exceptionHandler/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sys.main.pojo.ErrorInfo;
 
 /**
  * 处理异常
@@ -13,13 +15,25 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	@ExceptionHandler(value = Exception.class)
+	/*@ExceptionHandler(value = Exception.class)
 	public ModelAndView defultHandler(HttpServletRequest request,Exception e) throws Exception{
 		ModelAndView mav = new ModelAndView();
         mav.addObject("exception", e);
         mav.addObject("url", request.getRequestURL());
         mav.setViewName("errorPage");
 		return mav;
-	}
+	}*/
+	
+	
+	 	@ExceptionHandler(value = MyException.class)
+	    @ResponseBody
+	    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, MyException e) throws Exception {
+	        ErrorInfo<String> r = new ErrorInfo<>();
+	        r.setMessage(e.getMessage());
+	        r.setCode(ErrorInfo.ERROR);
+	        r.setData("Some Data");
+	        r.setUrl(req.getRequestURL().toString());
+	        return r;
+	    }
 
 }
